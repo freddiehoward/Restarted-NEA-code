dee3811987b55399b648de7fdfcc1d3b9c6d518e
freddiehoward <howard.f@etoncollege.org.uk>
2024-12-05 19:11:31 +0000

    added some comments to ui.py for readability

--- ui.py
+++ ui.py
@@ -4,9 +4,17 @@ from historical_and_predicted_data_plotter import plot_with_predictions
 
 
 st.title("Stock Predictor UI")
+#title of the window
+
 st.write("hello, world")
+#display a message
+
 user_ticker = st.text_input("input a ticker")
+#input field for user to input a ticker
+
 if st.button("make prediction"):
+    #button to make prediction
+    
     if len(user_ticker) > 0:
         data = fetch_stock_data(user_ticker)
         #train_LSTM(data[:-50])
